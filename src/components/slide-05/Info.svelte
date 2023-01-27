f77b7ca6b35f5ac79cd23f752f387b83bd7e4c9c
naram.dash <juho_kim@outlook.com>
2023-01-27 17:55:06 +0900

    today done

--- src/components/slide-05/Info.svelte
+++ src/components/slide-05/Info.svelte
@@ -6,7 +6,9 @@
 </script>
 
 <p>
-  The <code>{name}</code> package is {speed} fast. Download version {version} from
+  The <code>{name}</code> package is {speed} fast.
+  <br />
+  Download version {version} from
   <a href="https://www.npmjs.com/package/{name}">npm</a>
   and <a href={website}>learn more here</a>
 </p>
