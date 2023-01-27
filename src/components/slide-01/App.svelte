c4014cb2271e06e3ec91d8edf6de20b88003bdbd
naram.dash <juho_kim@outlook.com>
2023-01-27 14:47:15 +0900

    svelte file draft

---
+++ src/components/slide-01/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Nested from "./Nested.svelte";
+
+  let width = 500;
+  const src = `https://picsum.photos/${width}`;
+
+  console.log("GO~ SHOT!");
+</script>
+
+<label>
+  <input type="range" min="200" max="500" bind:value={width} />
+</label>
+
+<Nested {src} {width} />
---
+++ src/components/slide-01/Nested.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let src: string;
+  export let width: number;
+  export let alternative = "no alt text provided";
+</script>
+
+<figure>
+  <img {src} {width} alt={alternative} loading="lazy" />
+  <figcaption>
+    {alternative}
+  </figcaption>
+</figure>
+
+<style>
+  img {
+    border: 3px solid red;
+  }
+</style>
